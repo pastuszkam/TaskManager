8bbf315db33bc1c5b9ba10bba115d6741bc4fc02
tomasz.lyczko@evl.pl <lyczkul@interia.pl>
2017-06-28 19:42:14 +0200

    add methods for task

--- src/main/java/com/lyczkul/taskmanager/web/TaskController.java
+++ src/main/java/com/lyczkul/taskmanager/web/TaskController.java
@@ -1,21 +1,21 @@
 package com.lyczkul.taskmanager.web;
 
+import com.lyczkul.security.model.User;
+import com.lyczkul.security.service.impl.UserDataService;
 import com.lyczkul.taskmanager.model.Task;
 import com.lyczkul.taskmanager.repository.ProjectRepository;
 import com.lyczkul.taskmanager.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 /**
  * Created by Tomek on 2017-06-12.
  */
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/task")
 public class TaskController {
 
     @Autowired
@@ -24,13 +24,35 @@ public class TaskController {
     @Autowired
     private ProjectRepository projectRepository;
 
-    @RequestMapping(value = "/tasks/{user_id}", method = RequestMethod.GET)
+    @Autowired
+    private UserDataService userService;
+
+    @RequestMapping(value = "/user/{user_id}", method = RequestMethod.GET)
     public List<Task> tasks(@PathVariable(value = "user_id") Long userId) {
         return taskRepository.findAllByUserId(userId);
     }
 
-    @RequestMapping(value = "/tasks/project/{project_id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/project/{project_id}", method = RequestMethod.GET)
     public List<Task> projectTasks(@PathVariable(value = "project_id") Long projectId) {
         return taskRepository.findAllByProject(projectRepository.findOne(projectId));
+    }
+
+    @RequestMapping(value = "/{task_id}", method = RequestMethod.GET)
+    public Task task(@PathVariable(value = "task_id") Long taskId) {
+        return taskRepository.findOne(taskId);
+    }
+
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public Task create(@RequestBody Task task, Principal principal) {
+        User user = userService.getUser(principal);
+        task.setUser(user);
+        taskRepository.save(task);
+        return task;
+    }
+
+    @RequestMapping(value = "/delete/{task_id}", method = RequestMethod.DELETE)
+    public int delete(@PathVariable(value = "task_id") Long taskId) {
+        taskRepository.delete(taskId);
+        return 200;
     }
 }

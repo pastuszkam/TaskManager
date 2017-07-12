67bfda804e140a26edfba051bbc0e6f8c5a8a549
Maciej Pastuszka <maciej.pastuszka@evl.pl>
2017-07-12 16:56:02 +0200

    Commit testowy

--- src/main/wwwsrc/src/app/app.module.ts
+++ src/main/wwwsrc/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TestowyComponent } from './testowy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestowyComponent
   ],
   imports: [
     BrowserModule,
